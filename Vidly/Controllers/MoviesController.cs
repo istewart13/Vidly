679b481974079eecf8cf78f7eefa4d31b593b0ae
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-07-10 15:22:44 +0100

    Add index route

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -21,5 +21,15 @@ namespace Vidly.Controllers
             return Content("id=" + id);
         }
 
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
     }
 }
