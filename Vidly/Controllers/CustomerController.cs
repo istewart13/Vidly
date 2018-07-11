2dd0c188735d4b1ea5c2bda90dd78f655f91a41a
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-07-11 10:50:30 +0100

    Implement customer controller to pass customer view model for rendering

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -4,20 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class CustomerController : Controller
     {
-        // GET: Customer
+        [Route("Customers")]
         public ActionResult Index()
         {
-            var customers = new List<Customer>
-            {
-                new Customer {Name = "John Smith"},
-                new Customer {Name="Mary Williams"}
+            var customers = new List<Customer> {
+                new Customer { Name = "John Smith" },
+                new Customer { Name = "Mary Williams" }
             };
-            return View(customers);
+
+            var viewModel = new CustomerViewModel
+            {
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
     }
 }
