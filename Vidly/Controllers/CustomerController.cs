437c91406cbc1049e3db4063e5800e607a73b3b8
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-07-11 09:53:11 +0100

    Add customer controller

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "John Smith"},
+                new Customer {Name="Mary Williams"}
+            };
+            return View(customers);
+        }
+    }
+}
