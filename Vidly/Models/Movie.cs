1757c3c7de2544615153567c0c0307ea2923ccf0
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-07-13 10:23:36 +0100

    Add fields to movie class

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,10 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
-        public String Name { get; set; }
+        public string Name { get; set; }
+        [Required] public string Genre { get; set; }
+        [Required] public DateTime ReleaseDate { get; set; }
+        [Required] public DateTime DateAdded { get; set; }
+        [Required] public byte NumberInStock { get; set; }
     }
 }
